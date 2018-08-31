f99fc80c6240e1b1305ee62704e485860f9b642e
jfisac <jfisac@berkeley.edu>
2018-08-30 21:03:08 -0700

    Added analytical TEB in vertical direction and set up params for c++ objects
      - Define all parameters for tracker and planner objects.
      - Analytically determine TEB in vertical direction.
      - Save planner_params, tracker_params, bound_params for c++.
      - Slightly re-structured code for ease of reading.

--- ros/src/fastrack/matlab/ComputeTEB.m
+++ ros/src/fastrack/matlab/ComputeTEB.m
@@ -41,87 +41,82 @@
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%function ComputeTEB()
-% 1. Run Backward Reachable Set (BRS) with a goal
-%     uMode = 'min' <-- goal
-%     minWith = 'none' <-- Set (not tube)
-%     compTraj = false <-- no trajectory
-% 2. Run BRS with goal, then optimal trajectory
-%     uMode = 'min' <-- goal
-%     minWith = 'none' <-- Set (not tube)
-%     compTraj = true <-- compute optimal trajectory
-% 3. Run Backward Reachable Tube (BRT) with a goal, then optimal trajectory
-%     uMode = 'min' <-- goal
-%     minWith = 'zero' <-- Tube (not set)
-%     compTraj = true <-- compute optimal trajectory
-% 4. Add disturbance
-%     dStep1: define a dMax (dMax = [.25, .25, 0];)
-%     dStep2: define a dMode (opposite of uMode)
-%     dStep3: input dMax when creating your DubinsCar
-%     dStep4: add dMode to schemeData
-% 5. Change to an avoid BRT rather than a goal BRT
-%     uMode = 'max' <-- avoid
-%     dMode = 'min' <-- opposite of uMode
-%     minWith = 'zero' <-- Tube (not set)
-%     compTraj = false <-- no trajectory
-% 6. Change to a Forward Reachable Tube (FRT)
-%     add schemeData.tMode = 'forward'
-%     note: now having uMode = 'max' essentially says "see how far I can
-%     reach"
-% 7. Add obstacles
-%     add the following code:
-%     obstacles = shapeCylinder(g, 3, [-1.5; 1.5; 0], 0.75);
-%     HJIextraArgs.obstacles = obstacles;
+%% Problem parameters.
 
-% Do not compute optimal trajectory from an initial state.
-compTraj = false;
-
-%% Grid.
-grid_min = [ 0.01; -pi; -1; -1];  % Lower corner of computation domain
-grid_max = [ 0.5;  pi;  1;  1];   % Upper corner of computation domain
-N = [50; 35; 21; 21];             % Number of grid points per dimension
-pdDims = 2;                       % 2nd dimension is periodic
-g = createGrid(grid_min, grid_max, N, pdDims); % create state space grid
+% Gravity
+g = 9.81; % m/s^2
 
 
-%% Time vector.
+% ---- Planner parameters ----
+
+% Dubins turning rate bound.
+omega_max_ = pi; % rad/s
+
+% Dubins car speed.
+v_ = 1.0; % m/s
+
+% Vertical planner speed (for analytic vertical component of TEB).
+v_z_ = 0.5; % m/s
+
+
+% ---- Tracker parameters ----
+
+% Horizontal acceleration control bounds (via pitch/roll).
+pitch_roll_radius_ = 0.2; % rad;  ~10º --> ~0.2 rad --> ~0.2 g --> ~2 m/s^2
+a_max_ = g*tan(pitch_roll_radius_); % m/s^2; g*tan(0.2) = 1.99 m/s^2
+
+% Vertical and directional parameters (for analytic vertical component of TEB).
+yaw_rate_max_ = pi;  % rad/s
+thrust_min_ = g - 3; % m/s^2
+thrust_max_ = g + 6; % m/s^2
+
+% Horizontal disturbance bound (maximum radius of horizontal component).
+d_max_ = 0.5; % m/s^2
+
+% Vertical disturbance bound (symmetrical interval).
+d_z_max_ = 0.5; % m/s^2
+
+
+% ---- Differential game setup ----
+
+% State grid.
+grid_min = [ 0.01; -pi; -1; -1];  % Lower corner of computation domain.
+grid_max = [ 0.5;  pi;  1;  1];   % Upper corner of computation domain.
+N = [50; 35; 21; 21];             % Number of grid points per dimension.
+pdDims = 2;                       % 2nd dimension is periodic.
+g = createGrid(grid_min, grid_max, N, pdDims); % Create state space grid.
+
+% Time vector.
 t0 = 0;
 tMax = 0.5;
 dt = 0.05;
 tau = t0:dt:tMax;
 
-%% Problem parameters.
-% Tracker input bound.
-a_max_ = 2; % ~10º --> ~0.2 rad --> ~0.2 g --> ~2 m/s^2
-
-% Dubins input bound.
-omega_max = pi; % rad/s
-
-% Disturbance bound.
-d_max = 0.5; % m/s^2
-
-% Dubins car speed.
-v = 1.0; % m/s
-
-% surface function is the distance between the point mass and the Dubins car.
+% Payoff surface function: distance between the point mass and the Dubins car.
 data0 = g.xs{1};
 
-% Roles of the control and disturbance for the (original) surface function.
+% Roles of the control and disturbance for the (non-negated) surface function.
 uMode = 'min';
 dMode = 'max';
 
-%% --- Begin negated computation ---
+% HelperOC config. Do not compute optimal trajectory from an initial state.
+compTraj = false;
+
+
+%% Hamilton-Jacobi Computation of Tracking Error Bound (TEB)
+
+% ------------------------- Begin negated computation -------------------------
 % Since we are computing the *maximum* future payoff, but the LST only natively
 % supports the lower obstacle saturation, we need to negate the target surface
 % function.
 data0 = -data0;
 
-% The control and disturbance roles are switched: u maximizes *negated* distance.
+% Control and disturbance roles are switched: u maximizes *negated* distance.
 uMode = 'max';
 dMode = 'min';
 
 % Define dynamic system.
-mass_vs_dubins = Mass4DRelDubins([0, 0, 0, 0], a_max_, omega_max, d_max, v);
+mass_vs_dubins = Mass4DRelDubins([0, 0, 0, 0], a_max_, omega_max_, d_max_, v_);
 
 % Put grid and dynamic systems into schemeData.
 schemeData.grid = g;
@@ -133,32 +128,35 @@ schemeData.dMode = dMode;
 % Define type of Reachable Set to compute.
 minWith = 'zero'; % Compute reachable tube (for all time, not only final time)
 
-%% Compute value function.
-HJIextraArgs.visualize = true; %show plot
-HJIextraArgs.fig_num = 1; %set figure number
-HJIextraArgs.deleteLastPlot = true; %delete previous plot as you update
+% Set up value function computation options.
+HJIextraArgs.visualize = true; % Show plot
+HJIextraArgs.fig_num = 1; % Set figure number
+HJIextraArgs.deleteLastPlot = true; % Delete previous plot as you update
 
 HJIextraArgs.plotData.plotDims = [1 1 0 0];
 HJIextraArgs.plotData.projpt = 'min';
 HJIextraArgs.viewAngle = [0,90];
 
-%[data, tau, extraOuts] = ...
-% HJIPDE_solve(data0, tau, schemeData, minWith, extraArgs)
+% Compute value function.
 [data, tau2, ~] = ...
   HJIPDE_solve(data0, tau, schemeData, minWith, HJIextraArgs);
 
-% Un-negate value function to restore normal conditions..
+% Un-negate value function to restore normal conditions.
 data0 = -data0;
 data  = -data;
 uMode = 'min';
 dMode = 'max';
-% --- End negated computation ---
+% -------------------------- End negated computation --------------------------
 
 % Keep final step of value function computation only.
 data = squeeze(data(:,:,:,:,end));
 
-%% Plot surface function l(x) and value function V(x).
+
+%% Plotting
+
+% Plot surface function l(x) and value function V(x).
 [h_f, h_data, h_data0] = PlotValueXY(g,data,data0);
+
 
 %% Save data to load into FaSTrack.
 
@@ -177,47 +175,68 @@ num_cells = N;
 lower = grid_min;
 upper = grid_max;
 
-%------------------- Efficient-to-handle TEB approximation -------------------%
+%----------------------- Circumcircle TEB approximation -----------------------%
 % Compute circle approximation of the TEB in relative position space.
 % (This projects the velocity components out, which is done through a min.)
 data_project_to_position = min( min(data,[],4), [],3);
 
-% Create array of points in the TEB
+% Create array of points in the TEB.
 teb_points = [];
 for i_rho = 1:N(1)
-	for i_theta = 1:N(2)
-		if data_project_to_position(i_rho,i_theta) <= level_set_boundary_value
-			rho = g.vs{1}(i_rho);
-			theta = g.vs{2}(i_theta);
-			teb_points = [teb_points; rho, theta];
-		end
-	end
+    for i_theta = 1:N(2)
+        if data_project_to_position(i_rho,i_theta) <= level_set_boundary_value
+            rho = g.vs{1}(i_rho);
+            theta = g.vs{2}(i_theta);
+            teb_points = [teb_points; rho, theta];
+        end
+    end
 end
 
-% Compute circumscribed circle of the projected TEB (we know it is symmetric).
-R_c_sq = inf; % Initialize squared circumradius
-x_c = [];	  % Initialize circumcenter
+% Compute circumscribed circle of the projected TEB (we know it is symmetrical).
+R_c_sq = inf; % Initialize squared circumradius.
+x_c = [];     % Initialize circumcenter.
 rhos = teb_points(:,1);
 thetas = teb_points(:,2);
 res_x = 0.01; % 0.01 m resolution
 % Iterate over candidate center positions (linear pass).
 for x = -grid_max(1):res_x:grid_max(1)
-	% Compute radius for this center as distance to farthest point in the set.
-	d_sq = max( (rhos.*sin(thetas) ).^2 + (rhos.*cos(thetas) - x).^2 );
-	if d_sq < R_c_sq
-		R_c_sq = d_sq;
-		x_c = x;
-	end
+    % Compute radius for this center as distance to farthest point in the set.
+    d_sq = max( (rhos.*sin(thetas) ).^2 + (rhos.*cos(thetas) - x).^2 );
+    if d_sq < R_c_sq
+        R_c_sq = d_sq;
+        x_c = x;
+    end
 end
 R_c = sqrt(R_c_sq);
 
-%% TODO(@jaime): Define tracker_params, planner_params, bound_params consistent
-% with the Initialize() function in the appropriate class.
-tracker_params = []; % Control bounds for the tracker
-planner_params = []; % Control bounds for the tracker
-bound_params   = []; % TEB bounds
+% Semi-height of vertical TEB dimension.
+% Based on analytic double-integrator value function.
+% The limiting factor is the weakest acceleration capability of both directions.
+semi_height_ = v_z_^2 / (min(g - thrust_min_, thrust_max_ - g) - d_z_max_); % m
+
+% Control bound parameters for the planner: scalar_bound_interval, [min, max].
+planner_params = [-omega_max_, omega_max_];
+
+% Control bound parameters for the tracker: quadrotor_control_bound_cylinder,
+% [pitch-roll radius, min yaw rate, min thrust, max yaw rate, max thrust].
+tracker_params = [pitch_roll_radius_,...
+                  -yaw_rate_max_,...
+                  thrust_min_,...
+                  yaw_rate_max_,...
+                  thrust_max_];
+
+% TEB parameters: tracking_bound_cylinder
+% (parametrized TEB approximation: circumscribed circle).
+if abs(x_c) > res_x
+    warning(...
+        ['Circumcircle approximation to TEB is not centered on the origin.\n'...
+        'Current implementation assumes that it is. Check value function.'])
+end
+bound_params   = [R_c, semi_height_];
 
 % Save in .mat file.
-save value_function priority_upper priority_lower num_cells lower upper data
+save value_function ...
+     priority_upper priority_lower num_cells lower upper data ...
+     planner_params tracker_params bound_params
 
 % end
